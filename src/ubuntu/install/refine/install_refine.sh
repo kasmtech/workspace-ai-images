4b8f27ad4cec0f6489630884d26ad866d60dc3c5
Emrul Islam <emrul@emrul.com>
2025-05-07 16:35:32 +0100

    add AI extension

--- src/ubuntu/install/refine/install_refine.sh
+++ src/ubuntu/install/refine/install_refine.sh
@@ -2,6 +2,9 @@
 
 refine_version=${REFINE_VERSION:-"3.9.3"}
 
+apt-get update
+apt-get install -y unzip
+
 cd /opt
 wget -O refine.tar.gz "https://github.com/OpenRefine/OpenRefine/releases/download/${refine_version}/openrefine-linux-${refine_version}.tar.gz"
 tar xzf refine.tar.gz
@@ -12,11 +15,17 @@ cd openrefine
 cp refine refine.bak
 patch -f -p0 < "$(dirname "$0")/refine.patch"
 
-refine_logo=/opt/openrefine/webapp/modules/core/images/logo-gem-126.png
+
+# Install LLM AI extension
+cd /opt/openrefine/webapp/extensions
+wget -O "openrefine-llm-extension.zip" https://github.com/sunilnatraj/llm-extension/releases/download/v0.1.2.2/openrefine-llm-extension-0.1.2.zip
+unzip "openrefine-llm-extension.zip"
+rm "openrefine-llm-extension.zip"
 
 chown -R 1000:0 /opt/openrefine
 
 
+refine_logo=/opt/openrefine/webapp/modules/core/images/logo-gem-126.png
 cat >/usr/share/applications/openrefine.desktop<<EOL
 [Desktop Entry]
 Version=1.0
@@ -35,3 +44,10 @@ EOL
 chmod +x /usr/share/applications/openrefine.desktop
 chown kasm-user:kasm-user /usr/share/applications/openrefine.desktop
 ln -s /usr/share/applications/openrefine.desktop "$HOME/Desktop/openrefine.desktop"
+
+if [ -z ${SKIP_CLEAN+x} ]; then
+  apt-get autoclean
+  rm -rf \
+    /var/lib/apt/lists/* \
+    /var/tmp/*
+fi
