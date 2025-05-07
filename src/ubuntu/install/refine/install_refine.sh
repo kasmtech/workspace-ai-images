0121975f6e48d646ac00a36cd5673eac36c1b4df
Emrul Islam <emrul@emrul.com>
2025-05-07 15:38:33 +0100

    Add refine

--- src/ubuntu/install/refine/install_refine.sh
+++ src/ubuntu/install/refine/install_refine.sh
@@ -8,4 +8,30 @@ tar xzf refine.tar.gz
 mv "openrefine-${refine_version}" "openrefine"
 rm refine.tar.gz
 
+cd openrefine
+cp refine refine.bak
+patch -f -p0 < "$(dirname "$0")/refine.patch"
+
+refine_logo=/opt/openrefine/webapp/modules/core/images/logo-gem-126.png
+
 chown -R 1000:0 /opt/openrefine
+
+
+cat >/usr/share/applications/openrefine.desktop<<EOL
+[Desktop Entry]
+Version=1.0
+Type=Application
+Name=OpenRefine
+GenericName=Data Transformation Tool
+Comment=Data Transformation tool
+Icon=${refine_logo}
+Path="/opt/openrefine"
+Exec="/opt/openrefine/refine"
+Terminal=false
+MimeType=application/x-extension-ows;
+Categories=Science;Education;ArtificialIntelligence;DataVisualization;NumericalAnalysis;Java;
+Keywords=Scientific Visualization;Statistical Analysis;
+EOL
+chmod +x /usr/share/applications/openrefine.desktop
+chown kasm-user:kasm-user /usr/share/applications/openrefine.desktop
+ln -s /usr/share/applications/openrefine.desktop "$HOME/Desktop/openrefine.desktop"
