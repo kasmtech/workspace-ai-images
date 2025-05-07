4227f173a1ae1913e6328778c4980876294a5e8b
Emrul Islam <emrul@emrul.com>
2025-05-07 15:05:36 +0100

    Add refine

--- src/ubuntu/install/refine/install_refine.sh
+++ src/ubuntu/install/refine/install_refine.sh
@@ -3,8 +3,9 @@
 refine_version=${REFINE_VERSION:-"3.9.3"}
 
 cd /opt
-wget -O refine.tar.gz "https://openrefine.org/post_download?version=${refine_version}&platform=linux"
+wget -O refine.tar.gz "https://github.com/OpenRefine/OpenRefine/releases/download/${refine_version}/openrefine-linux-${refine_version}.tar.gz"
 tar xzf refine.tar.gz
+mv "openrefine-${refine_version}" "openrefine"
 rm refine.tar.gz
 
 chown -R 1000:0 /opt/refine
