e066c7d8b381aeccc8ac5dbdcea660290609cd2b
Emrul Islam <emrul@emrul.com>
2025-05-16 15:25:10 +0100

    Add zed

--- src/ubuntu/install/zed/install_zed.sh
+++ src/ubuntu/install/zed/install_zed.sh
@@ -4,6 +4,6 @@ set -ex
 cd "/home/kasm-default-profile"
 wget -O install.sh https://zed.dev/install.sh
 chmod +x "/home/kasm-default-profile/install.sh"
-chown 1000:0 "/home/kasm-default-profile/install.sh"
+chown -R 1000:0 "/home/kasm-default-profile"
 /bin/su -c "HOME=/home/kasm-default-profile /home/kasm-default-profile/install.sh" kasm-user
 rm "/home/kasm-default-profile/install.sh"
