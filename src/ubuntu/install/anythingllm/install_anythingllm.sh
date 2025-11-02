11794364d639eff3eb45a201ffbcbb29eea28aba
Teja Swaroop Pothala <tejaswaroop.pothala@kasmweb.com>
2025-11-02 23:46:31 +0000

    KASM-7910 fix anythingllm-desktop installation

--- src/ubuntu/install/anythingllm/install_anythingllm.sh
+++ src/ubuntu/install/anythingllm/install_anythingllm.sh
@@ -1,23 +1,46 @@
 #!/bin/bash
 set -ex
 
-runuser -l kasm-user -c "HOME=/tmp bash -c 'curl -fsSL https://cdn.anythingllm.com/latest/installer.sh | sh'"
-mv /tmp/AnythingLLMDesktop /opt/AnythingLLMDesktop
+APPIMAGE_URL="https://cdn.anythingllm.com/latest/AnythingLLMDesktop.AppImage"
+
+# Download AppImage
+mkdir -p /opt/AnythingLLMDesktop
+cd /opt/AnythingLLMDesktop
+curl -fsSL "$APPIMAGE_URL" -o /opt/AnythingLLMDesktop/AnythingLLMDesktop.AppImage
+chmod +x /opt/AnythingLLMDesktop/AnythingLLMDesktop.AppImage
+
+# Extract AppImage
+./AnythingLLMDesktop.AppImage --appimage-extract
+rm /opt/AnythingLLMDesktop/AnythingLLMDesktop.AppImage
 chown -R 1000:1000 "/opt/AnythingLLMDesktop"
 
-cat >/usr/share/applications/anythingllm.desktop<<EOL
-[Desktop Entry]
-Version=1.0
-Type=Application
-Name=AnythingLLM
-Icon=/opt/AnythingLLMDesktop/anythingllm-desktop/anythingllm-desktop.png
-Path="/opt/AnythingLLMDesktop/anythingllm-desktop"
-Exec=env APPDIR=/opt/AnythingLLMDesktop/anythingllm-desktop "/opt/AnythingLLMDesktop/start" --no-sandbox %f
-Comment=AnythingLLM LLM
-Categories=
-Terminal=false
-StartupNotify=true
+# Create launcher script
+cat >/opt/AnythingLLMDesktop/squashfs-root/launcher <<EOL
+#!/usr/bin/env bash
+export APPDIR=/opt/AnythingLLMDesktop/squashfs-root/
+/opt/AnythingLLMDesktop/squashfs-root/AppRun --no-sandbox "\$@"
 EOL
+chmod +x /opt/AnythingLLMDesktop/squashfs-root/launcher
+
+# Modify desktop file
+sed -i 's@^Exec=.*@Exec=/opt/AnythingLLMDesktop/squashfs-root/launcher@g' /opt/AnythingLLMDesktop/squashfs-root/*anythingllm*.desktop
+sed -i 's@^Icon=.*@Icon=/opt/AnythingLLMDesktop/squashfs-root/anythingllm-desktop.png@g' /opt/AnythingLLMDesktop/squashfs-root/*anythingllm*.desktop
+
+# Copy desktop file to appropriate locations
+cp /opt/AnythingLLMDesktop/squashfs-root/*anythingllm*.desktop $HOME/Desktop/anythingllm.desktop
+cp /opt/AnythingLLMDesktop/squashfs-root/*anythingllm*.desktop /usr/share/applications/anythingllm.desktop
+chmod +x $HOME/Desktop/anythingllm.desktop
 chmod +x /usr/share/applications/anythingllm.desktop
-chown 1000:1000 /usr/share/applications/anythingllm.desktop
-ln -s /usr/share/applications/anythingllm.desktop "$HOME/Desktop/anythingllm.desktop"
+
+
+# Cleanup for app layer
+chown -R 1000:0 $HOME
+find /usr/share/ -name "icon-theme.cache" -exec rm -f {} \;
+if [ -z ${SKIP_CLEAN+x} ]; then
+  apt-get autoclean
+  rm -rf \
+    /var/lib/apt/lists/* \
+    /var/tmp/* \
+    /tmp/*
+fi
+
